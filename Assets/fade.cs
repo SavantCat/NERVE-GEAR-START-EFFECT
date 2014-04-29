2b459ab211d60718b03b102072fdef8242a46951
Savant_Cat <scanf@me.com>
2014-04-29 10:38:07 +0900

    update

---
+++ Assets/fade.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fade : MonoBehaviour {
+	public Texture2D texture;
+	public int alpha = 255;
+	public	Color color;
+	// Use this for initialization
+	void  Awake() {
+		texture = new Texture2D( 1, 1, TextureFormat.ARGB32,false);
+		texture.SetPixel(0,0,Color.white);
+		texture.Apply();
+		color = new Color (0, 0, 0, alpha);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		color = new Color (0, 0, 0, alpha);
+		GUI.color = color;
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect( 0, 0, Screen.width, Screen.height ), texture );
+	}
+}
---
+++ Assets/test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	public float n;
+	public float r;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(r*Mathf.Cos(n),r*Mathf.Sin(n),0);
+	}
+}
